79fe2c4eb59a22311e5d5b0d33d58237ac4fa452
TarunRoy111 <134133257+TarunRoy111@users.noreply.github.com>
2025-01-03 17:19:48 +0530

    Update day 24.py

--- challanges/day 24.py
+++ challanges/day 24.py
@@ -1,1 +1,17 @@
-print('hellow')
+# write a program to generate random number and appending to the list
+import random
+n=int(input("Enter the elements:"))
+l=[]
+for i in range(n):
+    l.append(random.randint(0,n))
+print(l)
+
+# write a program to find the largest string between two strings
+s1="Hello"
+s2="Hii"
+largest=""
+if len(s1)<len(s2):
+    largest=s2 
+else:
+    largest=s1 
+print(largest)
